1073f79757b254ce0f673f8bcab954c920a79d89
Sarah Chen <magicmathmandarin@gmail.com>
2018-04-20 20:03:23 -0400

    Create Zigzagged flower.py

---
+++ Zigzagged flower.py
@@ -0,0 +1,8 @@
+#by 7 year old Vickie
+from turtle import *
+colors = ['red', 'purple', 'blue', 'green', 'yellow', 'orange']
+for x in range(7000):
+    pencolor(colors[x % 6])
+    width(x / 100 + 10)
+    forward(x)
+    left(100)
